e2275413053844c1df1bd575f29665c28419643d
felipematheus1337 <lipehbr@gmail.com>
2022-08-19 13:43:54 -0300

    hr worker implementation

---
+++ hr-worker/src/main/java/com/ms/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.ms.hrworker.repositories;
+
+import com.ms.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker,Long> {
+
+
+
+}
