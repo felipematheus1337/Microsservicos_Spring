79e88484bc6b86a40f0a08f1c8e816de02fc750a
felipematheus1337 <lipehbr@gmail.com>
2022-08-19 14:03:15 -0300

    hr-payroll implementation mocked

---
+++ hr-payroll/src/main/java/com/hrpayroll/services/PaymentService.java
@@ -0,0 +1,16 @@
+package com.hrpayroll.services;
+
+import com.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+
+    public Payment getPayment(long workerId,int days) {
+         return new Payment("Bob",200.0,days);
+
+    }
+
+
+}
