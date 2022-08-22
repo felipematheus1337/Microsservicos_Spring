8c1555ec7327df6858b97c5ad18e3db88e78a5a3
felipematheus1337 <lipehbr@gmail.com>
2022-08-22 13:00:06 -0300

    ribbon added

--- hr-worker/src/main/java/com/ms/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ms/hrworker/resources/WorkerResource.java
@@ -3,7 +3,10 @@ package com.ms.hrworker.resources;
 
 import com.ms.hrworker.entities.Worker;
 import com.ms.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository repository;
@@ -31,6 +41,10 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT = "+ env.getProperty("local.server.port"));
+
+
+
         Worker worker = repository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
